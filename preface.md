4ff3e10cd56d0ba2f31a67724fceaaad3760693a
Rasmus Hansen <rasmushansen@gmail.com>
2023-09-08 15:55:38 +0300

    Renamed bitcoin.json to terms.json

--- preface.md
+++ preface.md
@@ -3,7 +3,7 @@ Ordliste for Bitcoin- og økonomibegreber vedligeholdt af [Enogtyve.org](https:/
 
 Formålet med ordlisten er at sikre klar kommunikation, at undgå misforståelser og at hjælpe til med at bruge danske ord der er forståelige for nytilkomne.
 
-Bidrag til listen er kærkomne, og hvis du ønsker at tilføje ord eller forslå ændringsforslag gøres det ved at ændre i [bitcoin.json](bitcoin.json) og lave en 
+Bidrag til listen er kærkomne, og hvis du ønsker at tilføje ord eller forslå ændringsforslag gøres det ved at ændre i [terms.json](terms.json) og lave en 
 [Pull Request](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request). Holdet bag 
 Enogtyve vil hurtist muligt kigge den igennem og accepterer gode forslag.
  Bemærk at ændringer i andre filer automatisk vil blive overskrevet. 
--- scripts/convert.js
+++ scripts/convert.js
@@ -2,30 +2,25 @@ const fs = require('fs');
 const path = require('path');
 
 const prefaceInputPath = path.resolve(__dirname, '../preface.md');
-const bitcoinInputPath = path.resolve(__dirname, '../bitcoin.json');
-//const bitcoinMdOutputPath = path.resolve(__dirname, '../bitcoin.md');
+const jsonInputPath = path.resolve(__dirname, '../terms.json');
 const readmeMdOutputPath = path.resolve(__dirname, '../README.md');
 
-const bitcoinJson = JSON.parse(fs.readFileSync(bitcoinInputPath, 'utf8'));
+const json = JSON.parse(fs.readFileSync(jsonInputPath, 'utf8'));
 
-bitcoinJson.sort((a, b) => a.Term.localeCompare(b.Term));
+json.sort((a, b) => a.Term.localeCompare(b.Term));
 
 const tablePreamble = "| Begreb | På engelsk | Beskrivelse | Links |\n" +
     "|-|-|-|-|";
 
-const tableData = bitcoinJson.map(o => {
+const tableData = json.map(o => {
     const links = (o.Links || []).map(link => "[" + link.Title + "](" + link.Href + ")").join("  ")
     return "| " + o.Term.trim() + " | *" + o.English.trim() + "* | " + o.Description.trim() + " | " + links + " |";
 }).join("\n");
 
-const bitcoinTable = tablePreamble + "\n" + tableData;
-
-//console.log(bitcoinTable);
-
-//fs.writeFileSync(bitcoinMdOutputPath, bitcoinTable, 'utf8');
+const formattedTable = tablePreamble + "\n" + tableData;
 
 const prefaceMd = fs.readFileSync(prefaceInputPath, 'utf8');
-const readmeMd = prefaceMd + "\n\n## Bitcoinbegreber\n" + bitcoinTable;
+const readmeMd = prefaceMd + "\n\n## Begreber\n" + formattedTable;
 
 console.log(readmeMd);
 
